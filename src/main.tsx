01eac355cceb868a0686564592a2980839578e8f
KRSBX <firdaus.sati51@gmail.com>
2022-06-26 16:12:28 +0700

    chore: add 3d model assets

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ChakraProvider } from '@chakra-ui/react';
 import App from './App';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <div className="App">
+        <div className="container">
+          <App />
+        </div>
+      </div>
+    </ChakraProvider>
   </React.StrictMode>
 );
---
+++ src/utils/constant.ts
@@ -0,0 +1,10 @@
+export const rotationSettings = {
+  enabled: true,
+  rotationFactor: 5,
+};
+
+export const scaleSettings = {
+  enabled: true,
+  minScale: 0.3,
+  maxScale: 8,
+};
